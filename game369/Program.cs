672227ad6aa458330f860399ac8af831dac501d7
joygram <joygram@gmail.com>
2015-03-25 06:34:53 +0900

    add project

---
+++ game369/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game369
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //369 게임로직 
+            // 
+        }
+    }
+}
