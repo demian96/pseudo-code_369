7d4e281c3e6d78bd84a49e8d9d7846149a0675ea
demian96 <demian96@gmail.com>
2015-03-26 00:43:34 +0900

    add/ 369 슈도코드 add/ 3강 추가

---
+++ 3강/Chapter3/Chapter3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1000;
+            int b = 2000;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
---
+++ 3강/chapter3-2/chapter3-2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 200;
+            sbyte b = -126;
+            short c = 30000;
+            ushort d = 40000;
+            int e = 100000000;
+            uint f = 1000000000;
+            long g = 1000000000000000000;
+            ulong h = 10000000000000000000;
+
+            Console.WriteLine("a={0}, b={1}, c={2}, d={3}, e={4}, f={5}, g={6}, h={7}", a, b, c, d, e, f, g, h);
+        }
+    }
+}
