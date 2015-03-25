13a141f7944416bd12cf034192190015dd8588ab
joygram <joygram@gmail.com>
2015-03-26 01:03:50 +0900

    sizeof 체크 추가

--- 3강/Chapter3/ConsoleApplication1/Program.cs
+++ 3강/Chapter3/ConsoleApplication1/Program.cs
@@ -9,16 +9,19 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            byte a = 200;
-            sbyte b = -126;
-            short c = 30000;
-            ushort d = 40000;
-            int e = 100000000;
-            uint f = 1000000000;
-            long g = 1000000000000000000;
-            ulong h = 10000000000000000000;
+            byte a = 200; //  1 byte  - 255(0xff)
+            sbyte b = -126; // 부호 있는 1 byte (절반값만)
+            short c = 30000; // 4 byte
+            ushort d = 40000; // 부호없는 4byte
+            int e = 100000000; // 8byte
+            uint f = 1000000000; // 부호없는 8byte
+            long g = 1000000000000000000; // 16byte?
+            ulong h = 10000000000000000000; // 
 
             Console.WriteLine("a={0}, b={1}, c={2}, d={3}, e={4}, f={5}, g={6}, h={7}", a, b, c, d, e, f, g, h);
+
+            Console.WriteLine("sizeof byte={0}, sbyte={1}, short={2}, ushort={3}, int={4}, uint={5}, long={6}, ulong={7}", sizeof(byte), sizeof(sbyte), sizeof(short), sizeof(ushort), sizeof(int), sizeof(uint), sizeof(long), sizeof(ulong));
+
         }
     }
 }
