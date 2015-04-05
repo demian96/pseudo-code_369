75fd597fff57a91b3132fd6706a63a2c301a8fb9
demian96 <demian96@gmail.com>
2015-04-05 14:13:58 +0900

    add/ 7강 진행

---
+++ 369 슈도/write100_1/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0
+
+            int[] reading = new int[100] { 4, 9, 1, 0, 21, 12 };
+
+            for (int i = 0; i < reading.Length; i++)
+                Console.WriteLine("reading[" + i + "]={0}", reading[i]);
+        }
+    }
+}
