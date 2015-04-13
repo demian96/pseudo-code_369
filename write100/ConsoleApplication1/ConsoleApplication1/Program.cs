35f0bfac23205296890b6860a830dcdba16d585d
demian96 <demian96@gmail.com>
2015-04-13 23:06:28 +0900

    update/ 9강 진행, 100 출력 주석 달기 (+ 질문)

--- write100/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ write100/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,16 +9,19 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            int[] reading = new int[100];
+            int[] reading = new int[100]; // 100개의 reading 배열 선언
 
-			for(int i = 0; i < reading.Length; ++i)
+            for (int i = 0; i < reading.Length; ++i) 
+                // i는 0으로 선언,reading 배열의 Length가 위에서 선언된 배열 길이보다 작을 경우 +1씩 반복하여 배열의 위치를 지정
 			{
 				reading[i] = i+1; // i begin with 0
 			}
 
-           for (int b = 0; b < reading.Length; b++)
+           for (int b = 0; b < reading.Length; b++) 
+               // 0부터 시작하여 reading의 length가 위에서 선언된 배열 길이보다 작을 경우 +1씩 반복
+               //++i 와 b++ 의 차이를 모르겠음
 			{              
-               Console.WriteLine(reading[b]); 
+               Console.WriteLine(reading[b]); // 변수 b의 값을 출력
 			}
 
                
