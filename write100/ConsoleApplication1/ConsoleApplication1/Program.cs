d39bd2515087287d88f2a4da9cf3f0a7614af760
joygram <joygram@gmail.com>
2015-04-14 06:02:06 +0900

    Update Program.cs
    과제 추가

--- write100/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ write100/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+//숙제: ProcessArray()라는 함수를 작성하여 다음과 같은 문제를 해결하라
+//1. 배열에 200부터 짝수 100개를 넣고 출력하시오
+//2. 배열에 0부터 홀수 100개를 넣고 출력하시오 
+// 예: ProcessArray(시작값, 갯수, 짝수홀수여부)
+// 1번문제 ProcessArray(200, 100, true)
+// 2번문제 ProcessArray(0, 100, false)
+
 namespace ConsoleApplication1
 {
     class Program
