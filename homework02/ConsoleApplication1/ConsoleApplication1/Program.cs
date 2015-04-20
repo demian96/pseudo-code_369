ed1d9fb1c942d13484ecdc6e1f42a9f1fdd5ec10
demian96 <demian96@gmail.com>
2015-04-20 23:07:06 +0900

    add/ 두번째 숙제

---
+++ homework02/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//이름을 입력받아 "Hello" 이름을 출력하는 함수 제작
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("이름을 입력하세요: ");
+            string line = Console.ReadLine();
+            
+            Console.WriteLine("Hello "+ line);
+
+        }
+    }
+}
